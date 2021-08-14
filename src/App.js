e5bdc03d50db8d4a9c96fd6de15faae529a60523
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-14 18:02:38 +0300

    completed tour place project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Card from "./components/cards/Card";
+import "./App.css"
 
-function App() {
+const App = () => {
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <Card isVisible={isVisible} onMouseClick={onMouseClick}/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/cards/Card.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { data } from "../../helpers/Data";
+import "./Card.css";
+function Card() {
+//   const [showImage, setShowImage] = useState(true);
+  return (
+<div className="card-container">
+        {data.map((card) => {
+          return (
+            <div className="cards">
+              <div className="title">
+                <h2>{card.title}</h2>
+              </div>
+              <img src={card.image} alt={card.title} />
+              <div className= "card-over">
+                <p>{card.desc}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+
+
+
+    {/* <div className="card-container" onClick={() => setShowImage(!showImage)}>
+      {showImage ? (
+        <>
+          {data.map((card) => {
+            return (
+              <div className="cards" key={card.id}>
+                <img src={card.image} alt={card.title} />
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <>
+          {data.map((card) => {
+            return (
+              <div className="cards" key={card.id}>
+                <div className="title">
+                  <h2>{card.title}</h2>
+                </div>
+                <img src={card.image} alt={card.title} />
+                <div className="card-over">
+                  <p>{card.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  ); */}
+}
+export default Card;
